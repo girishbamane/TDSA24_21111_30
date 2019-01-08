a188dee9982a627d40b5d253f7eaa6be725c41b3
girishbamane <girishbamane1@gmail.com>
2019-01-08 22:18:02 +0530

    Cassandra insert working

--- src/main/scala/CassDBUtil.scala
+++ src/main/scala/CassDBUtil.scala
@@ -1,27 +1,9 @@
-import com.datastax.driver.core.TableMetadata
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import com.datastax.spark.connector._
-import com.datastax.spark.connector.streaming._
+import com.datastax.spark.connector.cql.CassandraConnector
 import org.apache.spark._
-import org.apache.spark.sql.SparkSession
-
-object CassDBUtil {
-  def main(args: Array[String]) {
-
-    val conf = new SparkConf(true)
-      .setMaster("local[2]")
-      .setAppName("DatastaxTests")
-      .set("spark.executor.memory", "1g")
-      .set("spark.cassandra.connection.host", "localhost")
-    val ssc = new SparkContext(conf);
-    val con = ssc.cassandraTable("sentimentdb", "sentiment")
-    val rdd = con.select("createdat", "hashtag", "sentiment", "tweet")
-    println("Records : " + rdd.count())
-    rdd.foreach(row => println("Row : " + " CreatedDate: " + row.getString(0) +
-      " HashTag: " + row.getString(1) +
-      " Sentiment: "+ row.getString(2)+
-      " Tweet: "+ row.getString(3)
-    ))
-
+object CassDBUtil extends App {
+  def saveDataToCassandra(ssc: SparkContext, createdAt: String, hashTag: String, sentiment: String, tweet: String): Boolean = {
+    val cassInsert = CassandraConnector.apply(ssc.getConf)
+    cassInsert.withSessionDo(session =>
+      session.execute("insert into sentimentdb.sentiment (createdat, hashtag, sentiment, tweet) values ('" + createdAt + "','" + hashTag + "','" + sentiment + "','" + tweet + "')").isExhausted)
   }
 }
